fef3d4f167c224c360975a3776abf5a8e39edab1
DDDDDevil <1445315879@qq.com>
2021-10-17 20:03:49 +0800

    实现贴吧显示帖子功能前端

--- src/main.js
+++ src/main.js
@@ -11,6 +11,24 @@ import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/app.css'
 import '@/permission'
 
+import relativeTime from 'dayjs/plugin/relativeTime';
+import format from 'date-fns/format'
+// 国际化
+import 'dayjs/locale/zh-cn'
+const dayjs = require('dayjs');
+
+// 相对时间插件
+dayjs.extend(relativeTime)
+
+dayjs.locale('zh-cn') // use locale globally
+dayjs().locale('zh-cn').format() // use locale in a specific instance
+
+Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
+
+Vue.filter('date', (date) => {
+  return format(new Date(date), 'yyyy-MM-dd')
+})
+
 Vue.use(Buefy)
 Vue.use(ElementUI);
 
