20246fa761141597943647a10e9b7225a542c868
wangh-site <1445315879@qq.com>
2022-06-27 00:20:51 +0800

    jjwt在jdk1.8运行报错修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -22,7 +22,7 @@ service.interceptors.request.use(
       // config.params = {'token': token}    // 如果要求携带在参数中
       // config.headers.token = token;       // 如果要求携带在请求头中
       // bearer：w3c规范
-      config.headers['Authorization'] = 'Bearer ' + getToken()
+      config.headers['Authorization'] = getToken()
     }
     return config
   },
